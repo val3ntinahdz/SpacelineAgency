4b960c563a3f2218bb57e2d6b8ad853b8b93aa14
val3ntinahdz <valentinaleonh8@gmail.com>
2025-07-16 16:44:56 -0600

    First look at the landing page (with basic styles) and animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
 const Home = () => {
     return (
     <div>
-      <section className="banner-section min-h-screen text-white flex flex-col items-start justify-center bg-banner px-8 py-16">
-        <div className="w-full max-w-4xl">
+      <section className="banner-section min-h-screen text-white flex flex-col items-start justify-center bg-banner px-8 pt-45 pb-16">
+        <div className="header w-full max-w-4xl">
           <h1 className="font-urbanist text-big tracking-tight leading-none text-left">
-            Creamos y Construimos<br />
-            <span className="block mt-0 italic gradient-text">Experiencias.</span>
+            No solo desarrollamos <span className="block mt-0 italic gradient-text">software. </span>
+            <br />
+            Creamos <span className="block mt-0 italic gradient-text">experiencias.</span>
           </h1>
 
-          <p className="text-tiny font-manrope tracking-tight text-left mt-4">
-            Estrategia, diseño y tecnología combinados<br />
-            para llevar tu negocio al siguiente nivel.
+
+          <p className="text-tiny font-manrope tracking-tight text-left">
+            Combinamos estrategia, diseño y tecnología a la medida para<br />
+            llevar tu negocio al siguiente nivel.
           </p>
         </div>
       </section>
